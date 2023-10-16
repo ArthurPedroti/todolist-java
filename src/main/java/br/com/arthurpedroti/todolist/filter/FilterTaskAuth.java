dc108a05faa61cecb522bdf0e544fc04ec1baa66
Arthur Pedroti <arthurpedroti@gmail.com>
2023-10-16 14:22:55 -0300

    finish app for deploy

--- src/main/java/br/com/arthurpedroti/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/arthurpedroti/todolist/filter/FilterTaskAuth.java
@@ -25,35 +25,42 @@ public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
-        throws ServletException, IOException {
-
-        // Pick authentication
-        var authorization = request.getHeader("Authorization");
-        
-        var authEncoded = authorization.substring("Basic".length()).trim();
-
-        byte[] authDecode = Base64.getDecoder().decode(authEncoded);
-
-        var authString = new String(authDecode);
-
-        String[] credentials = authString.split(":");
-
-        String username = credentials[0];
-        String password = credentials[1];
-
-        var user = this.userRepository.findByUsername(username);
-
-        if (user == null) {
-            response.sendError(401);
-        } else {
-            var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            throws ServletException, IOException {
             
-            if (passwordVerify.verified) {
-                filterChain.doFilter(request, response);
-            } else {
-                response.sendError(401);
-            }
+        var servletPath = request.getServletPath();
 
+        if(servletPath.startsWith("/tasks/")) {
+            // Pick authentication
+            var authorization = request.getHeader("Authorization");
+            
+            var authEncoded = authorization.substring("Basic".length()).trim();
+
+            byte[] authDecode = Base64.getDecoder().decode(authEncoded);
+
+            var authString = new String(authDecode);
+
+            String[] credentials = authString.split(":");
+
+            String username = credentials[0];
+            String password = credentials[1];
+
+            var user = this.userRepository.findByUsername(username);
+
+            if (user == null) {
+                response.sendError(401);
+            } else {
+                var passwordVerify = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+                
+                if (passwordVerify.verified) {
+                    request.setAttribute("idUser", user.getId());
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401);
+                }
+
+            }
+        } else {
+            filterChain.doFilter(request, response);
         }
     }
 }
